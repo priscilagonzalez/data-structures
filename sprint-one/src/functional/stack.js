e7ac5bda8cb20830ec7a0e8786d46cb4e7e68a46
thelensguy <thelensguy@gmail.com>
2019-12-11 16:43:45 -0800

    implemented stack - functional behavior throwing error

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,13 +6,42 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+
+    var greatest = Object.keys(storage)[0];
+    // define greatest variable
+    //var greatest = 0;
+   // if obj is empty
+   if (Object.keys(storage).length > 0) {
+   //if obj is not empty
+     //iterate over the obj
+     for (var key in storage) {
+       if (parseInt(key) > greatest) {
+         greatest = parseInt(key);
+       }
+      }
+      storage[greatest + 1] = value;
+    } else {
+      storage[greatest] = value;
+   }
   };
 
   someInstance.pop = function() {
+    var greatest = Object.keys(storage)[0];
+
+    for (var key in storage) {
+      if (parseInt(key) > greatest) {
+        greatest = parseInt(key);
+      }
+    }
+    var result = storage[greatest];
+    delete storage[greatest];
+    return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+

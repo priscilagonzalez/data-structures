e317e55e91c0e5523101d72cd461bb259a7a4036
thelensguy <thelensguy@gmail.com>
2019-12-12 16:57:35 -0800

    begin working on prototypal

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(Queue.queueMethods);
+  extend(obj, )
+
+
+
+
 };
+giraffeMaker.stuff = {}
+Class {
+  newGiraffe = Object.create(giraffeMaker.stuff)
+}
 
 var queueMethods = {};
 
 
+
+giraffeMaker.stuff.enqueue = function()
+giraffeMaker.stuff.dequeue = function()
+enqueue
+dequeue
+size
